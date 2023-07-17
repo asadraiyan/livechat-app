f46d7f42e7b937141e86bd1545678183f81a081b
Asad Raiyan <asadraiyan001@gmail.com>
2023-07-17 20:30:45 +0530

    created chat component added socket functionality

--- client/src/Components/Chat/Chat.js
+++ client/src/Components/Chat/Chat.js
@@ -1,11 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import socketIo from "socket.io-client";
+import "./Chat.css";
+import sendLogo from "../Images/send.png";
 import { user } from "../Join/Join";
 
+let socket;
+const ENDPOINT = "http://localhost:5000";
+
 const Chat = () => {
+  const [id, setId] = useState("");
+
+  const sendMessage = () => {
+    const message = document.getElementById("chatInput").value;
+    socket.emit("message", { message, id });
+    document.getElementById("chatInput").value = "";
+  };
+
+  useEffect(() => {
+    socket = socketIo(ENDPOINT, { transports: ["websocket"] });
+
+    socket.on("connect", () => {
+      alert("connected");
+      setId(socket.id);
+    });
+
+    console.log(socket);
+    socket.emit("joined", { user });
+
+    socket.on("welcome", (data) => {
+      console.log(data.user, data.message);
+    });
+    socket.on("userJoined", (data) => {
+      console.log(data.user, data.message);
+    });
+    socket.on("leave", (data) => {
+      console.log(data.user, data.message);
+    });
+
+    return () => {
+      socket.disconnect("disconnect");
+      socket.off();
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("sendMessage", (data) => {
+      console.log(data.user, data.message, data.id);
+    });
+    return () => {};
+  }, []);
+
   return (
-    <div>
-      <h1>{user}</h1>
-    </div>
+    <>
+      <div className="chat-page">
+        <div className="chat-container">
+          <div className="header"></div>
+          <div className="chat-box"></div>
+          <div className="input-box">
+            <input type="text" id="chatInput" />
+            <button className="send-btn" onClick={sendMessage}>
+              <img src={sendLogo} className="send-logo" alt="sendLogo" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -4,19 +4,46 @@ const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 
+const users = [{}];
+
 app.use(cors());
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000/",
     methods: ["GET", "POST"],
   },
 });
 
-io.on("connection", () => {
+io.on("connection", (socket) => {
   console.log("New connection");
+
+  socket.on("joined", ({ user }) => {
+    users[socket.id] = user;
+    console.log(`${user} has joined`);
+    socket.broadcast.emit("userJoined", {
+      user: "Admin",
+      message: `${users[socket.id]} has joined`,
+    });
+    socket.emit("welcome", {
+      user: "Admin",
+      message: `Welcome to the chat, ${users[socket.id]}`,
+    });
+
+    socket.on("message", ({ message, id }) => {
+      io.emit("sendMessage", { user: users[id], message, id });
+    });
+  });
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("leave", {
+      user: "Admin",
+      message: `${users[socket.id]} has left`,
+    });
+    console.log("User has left");
+  });
 });
 
 app.get("/", (req, res) => {
